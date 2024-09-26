e1f6bae40bf70aab3f0c7e81b353c4cf939d4195
kb150802 <bansalkushagra150802@gmail.com>
2024-09-26 14:58:43 +0530

    leaky bucket rate limiter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,13 @@
 import RateLimiter.RateLimiter;
-import RateLimiter.TokenBucketRateLimiter;
+import RateLimiter.LeakyBucketRateLimiter;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        RateLimiter rateLimiter = new TokenBucketRateLimiter(5,1);
+        RateLimiter rateLimiter = new LeakyBucketRateLimiter(5,1);
         int t = 100;
         while(t-- > 0) {
             System.out.println(rateLimiter.allowRequest());
-            Thread.sleep(1000);
+            Thread.sleep(100);
         }
 
     }
---
+++ src/main/java/RateLimiter/LeakyBucketRateLimiter.java
@@ -0,0 +1,47 @@
+package RateLimiter;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class LeakyBucketRateLimiter implements RateLimiter{
+    private final Integer capacity;
+    private final Integer leakRate;
+    private final Queue<Long> requestQueue;
+    private final AtomicLong lastLeakTime;
+    private final AtomicLong currentQueueSize;
+    public LeakyBucketRateLimiter(Integer capacity, Integer leakRate) {
+        this.capacity = capacity;
+        this.leakRate = leakRate;
+        this.requestQueue = new ConcurrentLinkedDeque<>();
+        this.lastLeakTime = new AtomicLong(System.nanoTime());
+        this.currentQueueSize = new AtomicLong(0);
+    }
+
+    @Override
+    public boolean allowRequest() {
+        performLeakage();
+        if(currentQueueSize.get() < capacity) {
+            if(currentQueueSize.incrementAndGet() <= capacity) {
+                requestQueue.offer(System.nanoTime());
+                return true;
+            }else{
+                currentQueueSize.decrementAndGet();
+            }
+        }
+        return false;
+    }
+    private void performLeakage() {
+        long currentTime = System.nanoTime();
+        long timeSinceLastLeak = currentTime - lastLeakTime.get();
+        long numberOfRequestsToLeak = (timeSinceLastLeak * leakRate) / TimeUnit.SECONDS.toNanos(1);
+        numberOfRequestsToLeak = Math.min(numberOfRequestsToLeak, currentQueueSize.get());
+        if(numberOfRequestsToLeak > 0) {
+            lastLeakTime.set(currentTime);
+        }
+        while(numberOfRequestsToLeak-->0) {
+            requestQueue.poll();
+            currentQueueSize.decrementAndGet();
+        }
+    }
+}
